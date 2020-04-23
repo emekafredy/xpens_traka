2278c540cda084b3c969df961b68e63f753eedff
emekafredy <chinedufrderick@gmail.com>
2020-04-23 03:45:15 +0100

    change image column name to file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_160237) do
+ActiveRecord::Schema.define(version: 2020_04_23_023713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_160237) do
     t.decimal "amount", precision: 16, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
+    t.string "file"
     t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_160237) do
     t.decimal "amount", precision: 16, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
+    t.string "file"
     t.index ["user_id"], name: "index_incomes_on_user_id"
   end
 
