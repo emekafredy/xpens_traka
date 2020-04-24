23ba575b4b38b1e039e5966b2f81bc28aa4249e8
emekafredy <chinedufrderick@gmail.com>
2020-04-24 19:52:24 +0100

    add custom error page

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module XpensTraka
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.exceptions_app = self.routes
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
